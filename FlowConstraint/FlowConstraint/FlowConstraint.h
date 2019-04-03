4d6b2d6249e125ff1e39abb94562443fa94c32ff
Muiz <muiz.idn@gmail.com>
2019-04-03 21:00:52 +0700

    Initialize project FlowConstraint

---
+++ FlowConstraint/FlowConstraint/FlowConstraint.h
@@ -0,0 +1,19 @@
+//
+//  FlowConstraint.h
+//  FlowConstraint
+//
+//  Created by PondokIOS on 03/04/19.
+//  Copyright © 2019 PondokIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowConstraint.
+FOUNDATION_EXPORT double FlowConstraintVersionNumber;
+
+//! Project version string for FlowConstraint.
+FOUNDATION_EXPORT const unsigned char FlowConstraintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowConstraint/PublicHeader.h>
+
+
